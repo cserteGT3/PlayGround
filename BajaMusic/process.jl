e9798b2d579193471b1a346b8fc0f9cea62f5a71
Tamás Cserteg <csertegt@gmail.com>
2021-01-18 15:53:25 +0100

    finish extracting script

--- BajaMusic/process.jl
+++ BajaMusic/process.jl
@@ -1,12 +1,46 @@
 using JSON
+using Logging
 
-f = read("message_1.json", String)
-fd = JSON.parse(f)
-msgs = fd["messages"]
+# msgs = readjson("message_1.json")
+function readjson(fname)
+	f = read(fname, String)
+	fd = JSON.parse(f)
+	return fd["messages"]
+end
 
-
-for msg in msgs
-	if msgs contains "spotify" or "apple" or youtube
-		 
+function collectlinks(messages)
+	links = String[]
+	for i in 1:size(messages, 1)
+		msg = messages[i]
+		haskey(msg, "content") || continue
+		msgc = msg["content"]
+		if occursin("spotify.", msgc) || occursin("music.apple", msgc) || occursin("youtube.", msgc)
+			if occursin("\n", msgc)
+				#@show i
+				msgcs = split(msgc, "\n")
+				for msgc in msgcs
+					occursin(".com", msgc) && push!(links, msgc)
+				end
+			else
+				push!(links, msgc)
+			end
+		end
 	end
-	
+	return links
+end
+
+function writehtml(links, endfile)
+	open(endfile, "w") do io
+		println(io, "<!DOCTYPE html>\n<html>\n<body>")
+		for l in links
+			println(io, """<p><a href="$l">$l</a></p>""")
+		end
+		println(io, "</body>\n</html>")
+	end
+	@info "Finished writing $(length(links)) links."
+end
+
+# completeproc("message_1.json")
+function completeproc(fname)
+	readjson(fname) |> collectlinks |> x -> writehtml(x, "links.html")
+end
